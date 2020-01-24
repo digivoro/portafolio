5259ce9ae4da2e9a575b810290897f414db1ff77
Felipe Castro Richter <nkardia@gmail.com>
2020-01-24 15:42:44 -0300

    Arreglo velocidad del efecto typewriter

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,65 +2,70 @@ $(function() {
   $('[data-toggle="tooltip"]').tooltip();
 });
 
-var TxtType = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = "";
-  this.tick();
-  this.isDeleting = false;
-};
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// Old Typewriter ////////////////////////////////////////////////////////////////////////////////
 
-TxtType.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
+// var TxtType = function(el, toRotate, period) {
+//   this.toRotate = toRotate;
+//   this.el = el;
+//   this.loopNum = 0;
+//   this.period = parseInt(period, 10) || 2000;
+//   this.txt = "";
+//   this.tick();
+//   this.isDeleting = false;
+// };
 
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
+// TxtType.prototype.tick = function() {
+//   var i = this.loopNum % this.toRotate.length;
+//   var fullTxt = this.toRotate[i];
 
-  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+//   if (this.isDeleting) {
+//     this.txt = fullTxt.substring(0, this.txt.length - 1);
+//   } else {
+//     this.txt = fullTxt.substring(0, this.txt.length + 1);
+//   }
 
-  var that = this;
-  var delta = 200 - Math.random() * 100;
+//   this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
 
-  if (this.isDeleting) {
-    delta /= 2;
-  }
+//   var that = this;
+//   var delta = 200 - Math.random() * 100;
 
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === "") {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-  }
+//   if (this.isDeleting) {
+//     delta /= 2;
+//   }
 
-  setTimeout(function() {
-    that.tick();
-  }, delta);
-};
+//   if (!this.isDeleting && this.txt === fullTxt) {
+//     delta = this.period;
+//     this.isDeleting = true;
+//   } else if (this.isDeleting && this.txt === "") {
+//     this.isDeleting = false;
+//     this.loopNum++;
+//     delta = 500;
+//   }
 
-window.onload = function() {
-  var elements = document.getElementsByClassName("typewrite");
-  for (var i = 0; i < elements.length; i++) {
-    var toRotate = elements[i].getAttribute("data-type");
-    var period = elements[i].getAttribute("data-period");
-    if (toRotate) {
-      new TxtType(elements[i], JSON.parse(toRotate), period);
-    }
-  }
-  // INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-  document.body.appendChild(css);
-};
+//   setTimeout(function() {
+//     that.tick();
+//   }, delta);
+// };
 
+// window.onload = function() {
+//   var elements = document.getElementsByClassName("typewrite");
+//   for (var i = 0; i < elements.length; i++) {
+//     var toRotate = elements[i].getAttribute("data-type");
+//     var period = elements[i].getAttribute("data-period");
+//     if (toRotate) {
+//       new TxtType(elements[i], JSON.parse(toRotate), period);
+//     }
+//   }
+//   // INJECT CSS
+//   var css = document.createElement("style");
+//   css.type = "text/css";
+//   css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+//   document.body.appendChild(css);
+// };
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// Navbar behavior on scroll & hover /////////////////////////////////////////////////////////////
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
@@ -83,6 +88,71 @@ $("#navbar").hover(
   }
 );
 
-// $("#navbar").mouseout(function() {
-//   document.getElementById("navbar").style.top = "-50px";
-// });
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// Brad Typewriter ///////////////////////////////////////////////////////////////////////////////
+class TypeWriter {
+  constructor(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = "";
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+  }
+
+  type() {
+    // Current index of word
+    const current = this.wordIndex % this.words.length;
+    // Get full text of current word
+    const fullTxt = this.words[current];
+
+    // Check if deleting
+    if (this.isDeleting) {
+      // Remove char
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      // Add char
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    // Insert txt into element
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    // Initial Type Speed
+    let typeSpeed = 100;
+
+    if (this.isDeleting) {
+      typeSpeed /= 2;
+    }
+
+    // If word is complete
+    if (!this.isDeleting && this.txt === fullTxt) {
+      // Make pause at end
+      typeSpeed = this.wait;
+      // Set delete to true
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      this.isDeleting = false;
+      // Move to next word
+      this.wordIndex++;
+      // Pause before start typing
+      typeSpeed = 500;
+    }
+
+    setTimeout(() => this.type(), typeSpeed);
+  }
+}
+
+// Init On DOM Load
+document.addEventListener("DOMContentLoaded", init);
+
+// Init App
+function init() {
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
+  // Init TypeWriter
+  new TypeWriter(txtElement, words, wait);
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////
